6041a649c6ed00d3f976615212dd5299b70c1877
pinkig15 <pinkigautam15@gmail.com>
2019-05-20 11:19:15 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/api/customers", (req, res) => {
+  const customers = [
+    { id: 1, firstName: "John", lastName: "Doe" },
+    { id: 2, firstName: "Brad", lastName: "Traversy" },
+    { id: 3, firstName: "Mary", lastName: "Swanson" }
+  ];
+
+  res.json(customers);
+});
+
+const port = 5000;
+
+app.listen(port, () => `Server running on port ${port}`);
+
+
+const express = require("express");
+
+const app = express();
+
+app.get('/api/customers', (req, res=> {
+  const customers = [
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+  ]
+  res.json(customers)
+}))
+
+const port = 5000;
+app.listen(port, () => `listening on port....${port}`)
